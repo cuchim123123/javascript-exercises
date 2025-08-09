9847c5a3bfd9a44dfb38af4f30bec2e246eccc29
Bao Ngo <ngbao2485@gmail.com>
2025-08-09 20:58:04 +0700

    Finish js 07

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = '';
+    if(num < 0) return 'ERROR';
+    while(num>0){
+        output+= string;
+        num--;
+    }
+    return output;
 };
 
 // Do not edit below this line
