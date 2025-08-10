d64ca120d5f322bf5ddd71a85a742a9275d8363c
Bao Ngo <ngbao2485@gmail.com>
2025-08-10 22:54:05 +0700

    Finish js 09 (brute force)

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let sum = 0;
+    let smaller = int1>=int2 ? int2 : int1;
+    let larger = int1>=int2 ? int1 : int2;
+    if(!Number.isInteger(int1)|| !Number.isInteger(int2) || int1<0 || int2<0){
+        return "ERROR";
+    }
+    for(let i = smaller; i <= larger; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
