cc8ae199eba30e9040abb64609a87fc9f6aa8d78
Bao Ngo <ngbao2485@gmail.com>
2025-08-16 19:04:45 +0700

    Finish js 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  return num.reduce((acc, cur)=> acc+(+cur), 0);
 };
 
-const multiply = function() {
+const multiply = function(num) {
+  return num.reduce((acc, cur)=> acc*(+cur), num.length!==0 ? 1:0);
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a === 0) return 1;
+  else return a*factorial(a-1);
 };
 
 // Do not edit below this line
