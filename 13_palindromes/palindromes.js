22a46b7a52f5f422b8ca3b0bd2696b5f2316e7b4
Bao Ngo <ngbao2485@gmail.com>
2025-08-19 01:41:38 +0700

    Finish js 13 (brute)

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    return str.trim().split("").filter(ch => isAlphabetical(ch)).join("").toLowerCase() === str.trim().split("").filter(ch =>isAlphabetical(ch)).reverse().join("").toLowerCase();
 };
 
+function isAlphabetical(ch){
+    return ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789").includes(ch);
+}
 // Do not edit below this line
 module.exports = palindromes;
