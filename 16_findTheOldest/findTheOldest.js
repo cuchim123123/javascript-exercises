7be8e699d85af71630bafb53a2dc21fd082ad690
Bao Ngo <ngbao2485@gmail.com>
2025-08-19 19:52:01 +0700

    Enhance

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, person) => {
+        const oldestAge = (oldestPerson.yearOfDeath || getYear()) - oldestPerson.yearOfBirth
+        const age = (person.yearOfDeath || getYear()) - person.yearOfBirth
+        if(age>oldestAge){
+            return person;
+        }
+        return oldestPerson;
+    }, people[0]);
 };
+
+function getYear(){
+    return new Date().getFullYear();
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
