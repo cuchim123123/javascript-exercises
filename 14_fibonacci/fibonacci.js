38f15b9ce04b9394ce99f26aa02a8cb849c9078e
Bao Ngo <ngbao2485@gmail.com>
2025-08-19 17:20:54 +0700

    Finish js 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num<0) return 'OOPS';
 
+    let ficArr = [0, 1];
+    if(num<2) return ficArr[num];
+    for(let i=2; i<=num; i++){
+        ficArr[i] = ficArr[i-1] +ficArr[i-2];
+    }
+    return ficArr[num];
 };
 
 // Do not edit below this line
