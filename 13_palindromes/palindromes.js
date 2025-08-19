2ce01da85838d1503997dcb55ecbb10d5be08ef2
Bao Ngo <ngbao2485@gmail.com>
2025-08-19 17:10:36 +0700

    Enhance

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,9 +1,18 @@
 const palindromes = function (str) {
-    return str.trim().split("").filter(ch => isAlphabetical(ch)).join("").toLowerCase() === str.trim().split("").filter(ch =>isAlphabetical(ch)).reverse().join("").toLowerCase();
+    let strArr = str
+        .trim()
+        .toLowerCase()
+        .split("")
+        .filter(ch => isAlphabetical(ch))
+        ;
+    return strArr.join("") === strArr.reverse().join("");
 };
 
 function isAlphabetical(ch){
-    return ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789").includes(ch);
+    return ("abcdefghijklmnopqrstuvwxyz0123456789").includes(ch.toLowerCase());
 }
+
+
+
 // Do not edit below this line
 module.exports = palindromes;
